67bf91434819be5606c4e8043d891df9b247931d
Lara Ayumi <83134763+ayumilh@users.noreply.github.com>
2023-06-26 21:10:26 -0300

    responsivo, navbar e sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,24 +1,32 @@
-CREATE DATABASE Cadastro;
-use Cadastro;
+CREATE DATABASE cadastro;
 
--- create
-CREATE TABLE projeto(
-    pin int(5) PRIMARY KEY UNIQUE,
-    id_cliente int(2),
-    nome_cliente VARCHAR(40),
-    data DATE,
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
-);
+USE cadastro;
 
+-- Tabela cliente
 CREATE TABLE cliente (
-    id int(2) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(30) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    rg VARCHAR(12) NOT NULL,
+    telefone1 VARCHAR(15) NOT NULL,
+    telefone2 VARCHAR(15),
+    endereco_atual VARCHAR(200) NOT NULL,
+    endereco_projeto VARCHAR(200) NOT NULL,
+    fatura_disponivel VARCHAR(10) NOT NULL
+);
+
+-- Tabela projeto
+CREATE TABLE projeto (
+    id_cliente INT,
+    pin_projeto VARCHAR(5) PRIMARY KEY NOT NULL,
+    planta VARCHAR(200) NOT NULL,
+    empreendimento VARCHAR(100) NOT NULL,
+    M2 DECIMAL(3, 2) NOT NULL,
+    previsao_entrega VARCHAR(10) NOT NULL,
+    FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente)
 );
 
 
-
--- select 
 SELECT * FROM cliente WHERE cpf = '58962188856';
 
 SELECT * FROM projeto WHERE pin = '11111';
